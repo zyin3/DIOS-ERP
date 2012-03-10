f0493bddf80660bbc7758e3d566c4d0891c8be5d
Ray <gliese.q@gmail.com>
2012-03-10 08:58:54 -0800

    add dynamicly calculated project path

--- src/eclaim/settings.py
+++ src/eclaim/settings.py
@@ -1,5 +1,7 @@
 # Django settings for eclaim project.
 
+import os.path as path
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -8,6 +10,8 @@ ADMINS = (
 )
 
 MANAGERS = ADMINS
+
+PROJECT_ROOT = path.dirname(path.dirname(__file__))
 
 DATABASES = {
     'default': {
@@ -72,8 +76,8 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    
-    "DIOS/src/static",
+
+    path.join(PROJECT_ROOT, 'static'),
 )
 
 # List of finder classes that know how to find static files in
@@ -108,7 +112,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "DIOS/src/template/",
+    path.join(PROJECT_ROOT, 'template'),
 )
 
 INSTALLED_APPS = (
