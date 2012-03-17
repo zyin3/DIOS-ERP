94cfbddd62ffe1c12eed5b4439a02a37b8fd1956
Qin Sun <danney.sun@gmail.com>
2012-03-16 22:54:17 -0400

    commit check in

--- src/eclaim/urls.py
+++ src/eclaim/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.defaults import patterns, include, url
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
-
+# check in test
 urlpatterns = patterns('',
     #index page
     url(r'^eclaim/$', 'eclaim.views.index'),
